1dd7d17a9589990c6ced03fca97bfadbd47a9e4b
esra <esra_izgi@hotmail.com>
2022-03-19 14:41:57 -0700

    first commit

---
+++ src/test/java/jenkins/JenkinsTests.java
@@ -0,0 +1,54 @@
+package jenkins;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class JenkinsTests {
+
+
+
+    @Test
+    public void apiTest(){
+
+        RequestSpecification spec  = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com").build();
+
+        //Set the base url and path params
+        spec.pathParams("first", "booking", "second",3);
+
+
+        //Send the Get request and get the response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+
+
+        response.prettyPrint();
+
+        //validation
+        // responseun data type map ile uygun olmali o yuzden hashmap yazdik. Class ta bu classtan demek icin
+        // hashmap is a implementing from his interface.
+        Map<String, Object> actualData = response.as(HashMap.class);
+
+        Map<String, Object> bookingdates = (Map)actualData.get("bookingdates");
+
+
+        assertEquals("Sally", actualData.get("firstname"));
+        assertEquals("Jones", actualData.get("lastname"));
+        assertEquals(844, actualData.get("totalprice"));
+        assertEquals(true, actualData.get("depositpaid"));
+
+        assertEquals("2019-05-01", bookingdates.get("checkin"));
+        assertEquals("2019-11-02", bookingdates.get("checkout"));
+
+    }
+
+
+
+}
